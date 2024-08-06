8519cb1a51cd61716b80de46a9e6d0dc3e16a53a
programador web <programadorwebteste@gmail.com>
2024-08-05 23:23:00 -0300

    Projeto Demander

---
+++ app/Http/Controllers/ConvertController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+
+class ConvertController extends Controller
+{
+    public function convert(Request $request) {
+        
+        #Verifica se o valor solicitado é um número
+        if (is_numeric($request -> num) and ($request -> num) > 0 and ($request -> num) < 4000) {
+            $valor = $request -> num;
+            $resultado = $this ->convert_romanos((int)$valor);
+            return view('convert')->with(['resultado' => $resultado, 'valor' => $valor]);
+
+        #Verifica se o valor solicitado é um algarismo romano
+        } elseif(preg_match("/^M{0,3}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$/", strtoupper($request -> num))){
+            $valor = strtoupper($request -> num);
+            $resultado = $this->convert_real($request -> num);
+            return view('convert')->with(['resultado' => $resultado, 'valor' => $valor]);
+        } else {
+            $error = 'Valor Inválido';
+            return view('convert')->with(['error' => $error]);
+        }
+    }
+
+    #Base de algarismo romanos
+    public $base_romanos = ['M' => 1000,'CM' => 900,'D' => 500,'CD' => 400,'C' => 100,'XC' => 90,'L' => 50,'XL' => 40,'X' => 10,'IX' => 9,'V' => 5,'IV' => 4,'I' => 1];
+
+    #Método para converter para numeros romanos
+    public function convert_romanos($numero) {
+        $resultado = '';
+        foreach ($this -> base_romanos as $romano => $valor) {
+            while ($numero >= $valor) {
+                $resultado .= $romano;
+                $numero -= $valor;
+            }
+        }
+        return $resultado;
+    }
+
+    #Método para converter para numeros reais
+    public function convert_real($numero) {
+        $resultado = 0;
+        $romano = strtoupper($numero);
+        $x = 0;
+        while ($x < strlen($romano)) {
+            $caracter_romano = $romano[$x];
+            $proximo = ($x + 1 < strlen($romano)) ? $romano[$x + 1] : null;
+            if ($proximo && isset($this->base_romanos[$caracter_romano . $proximo])) {
+                $resultado += $this->base_romanos[$caracter_romano . $proximo];
+                $x += 2;
+            } else {
+                $resultado += $this->base_romanos[$caracter_romano];
+                $x++;
+            }
+        }
+        return $resultado;
+    }
+
+    public function index() {
+        return view('convert');
+    }
+
+    
+}
